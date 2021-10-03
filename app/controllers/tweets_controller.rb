7842d4a0c73920cea68c8d2f738dcd1cb3db8c2a
Ubuntu <ubuntu@ip-172-31-69-107.ec2.internal>
2021-10-03 10:34:30 +0000

    パスの書き換え、テンプレ作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   def create
     @Tweet = Tweet.new(message: params[:tweet][:message],tdate: Time.current)
     if @Tweet.save
-    flash[:notice] = '1レコード追加しました'
+    flash[:notice] = 'ツイートしました'
     redirect_to '/'
     else
       render 'new'
@@ -20,7 +20,7 @@ class TweetsController < ApplicationController
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy
-    flash[:notice] = '該当レコードを削除しました'
+    flash[:notice] = 'ツイートを削除しました'
     redirect_to '/'
   end
   
@@ -35,7 +35,7 @@ class TweetsController < ApplicationController
   def update
     tweet = Tweet.find(params[:id])
     if tweet.update(message: params[:tweet][:message])
-    flash[:notice] = '該当レコードを更新しました'
+    flash[:notice] = 'ツイートを更新しました'
     redirect_to '/'
     else
       render 'edit'
